0e5501330d6806cc470e2334030bf3b911379241
nape <najinshah.pearson@gmail.com>
2023-06-06 22:23:16 +0530

    pinia added

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import './style.css'
 import App from './App.vue'
 const app = createApp(App)
 
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+app.use(pinia)
 app.mount('#app')
